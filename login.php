8aef560b66702cfb6dd67d7e031b6dbba1d8d036
Sarp Ulas Kaya <sarpkaya2000@gmail.com>
2021-05-04 22:44:01 +0300

    Successfully implemented sign-up
    -Users successfully added after registration with the correct attributes and specilizations

--- login.php
+++ login.php
@@ -3,20 +3,20 @@ include("config.php");
 session_start();
 function logIn($mysqli) {
 	$username = $_POST['name'];
-	$id = $_POST['password'];
-	$_SESSION['user_id'] = $id;
-	$query = " select * from student where sname = '$username' and sid = '$id'";
+	$password = $_POST['password'];
+	//$_SESSION['user_id'] = $id;
+	$query = " select * from users where name = '$username' and password = '$password'";
 	if ($result = $mysqli->query($query)) {
 		if ($result->num_rows==1) {
 			$_SESSION['login_user'] = $username;
 			header("location: welcome.php");
 		}
 		else {
-			header("location: index.php");
+			header("location: index.html");
 		}
 	}
 	else {
-		header( "location: index.php");
+		header( "location: index.html");
 	}
 }
 if (isset($_POST['name']) && isset($_POST['password']) ) {
--- signup.php
+++ signup.php
@@ -1,3 +1,6 @@
+<?php
+require_once('config.php')
+?>
 <DOCTYPE! html>
 <html>
 <head>
@@ -10,54 +13,122 @@
     Congrats! Welcome to BookWorm Family
   </h1>
   <div>
+      <?php
+      if (isset($_POST['signup'])) {
+          echo 'isset entered.';
+          $name = $_POST['firstName'];
+          $name .= " ";
+          $name .= $_POST['lastName'];
+          $email = $_POST['email'];
+          $password = $_POST['password'];
+          $birthday = $_POST['birthday'];
+          $gender = $_POST['gender'];
+          /*
+          $query = "INSERT INTO users( user_id, name, mail_address, password, last_login,
+                             created_at, birthday, gender ) VALUES (LAST_INSERT_ID(), ?, ?, ?, ?, ?, ?, ?)";
+          $stmtinsert = $mysqli->prepare($query);
+          $stmtinsert->bind_param($name, $email, $password, $current_date, $current_date,
+                                    $birthday, $gender);
+          $result = $stmtinsert->execute();
+          */
+
+          $query = "INSERT INTO users( user_id, name, mail_address, password, last_login,
+                             created_at, birthday, gender ) VALUES (LAST_INSERT_ID(),'$name', '$email',
+                                                                    '$password', NOW(), NOW(),
+                                                                    '$birthday', '$gender')";
+          $stmtinsert = $mysqli->prepare($query);
+          $result = $stmtinsert->execute();
+          $stmtinsert->close();
+          if($result) {
+              echo 'Successfully inserted new user.';
+          }
+          switch ($_POST['accountType']) {
+              case 'librarian' :
+                  echo 'librarian';
+                  $libQuery = "INSERT INTO librarian_account( user_id ) SELECT user_id FROM users ORDER BY user_id DESC LIMIT 1";
+                  $stmtinsert = $mysqli->prepare($libQuery);
+                  $result = $stmtinsert->execute();
+                  $stmtinsert->close();
+                  if($result) {
+                      echo 'Successfully inserted new librarian.';
+                  }
+                  break;
+              case 'author' :
+                  echo 'author';
+                  $autQuery = "INSERT INTO author_account( user_id ) SELECT user_id FROM users ORDER BY user_id DESC LIMIT 1";
+                  $stmtinsert = $mysqli->prepare($autQuery);
+                  $result = $stmtinsert->execute();
+                  $stmtinsert->close();
+                  if($result) {
+                      echo 'Successfully inserted new author.';
+                  }
+                  break;
+              case 'reader' :
+                  echo 'reader';
+                  $readQuery = "INSERT INTO reader_account( user_id ) SELECT user_id FROM users ORDER BY user_id DESC LIMIT 1";
+                  $stmtinsert = $mysqli->prepare($readQuery);
+                  $result = $stmtinsert->execute();
+                  $stmtinsert->close();
+                  if($result) {
+                      echo 'Successfully inserted new reader.';
+                  }
+                  break;
+          }
+      }
+      ?>
+  </div>
+  <form class="button" action="signup.php" method="post">
+  <div>
     <input type="text"
            id="firstName"
+           name="firstName"
            placeholder="First name">
     <br></br>
     <input type="text"
            id="lastName"
+           name="lastName"
            placeholder="Last name"
            style="margin-top: 5px;">
     <br></br>
     <input type="text"
            id="email"
+           name="email"
            placeholder="Email"
            style="margin-top: 5px;">
     <br></br>
     <input type="text"
            id="password"
+           name="password"
            placeholder="Password"
            style="margin-top: 5px;">
     <br></br>
     <input type="text"
            id="confirmPassword"
+           name="confirmPassword"
            placeholder="Confirm Password"
            style="margin-top: 5px;">
     <br></br>
     <input type="date"
            id="birthday"
+           name="birthday"
            placeholder="Birthday"
            style="margin-top: 5px;">
     <br></br>
-    <select id="gender" placeholder="Gender" style="margin-top: 5px">
+    <select id="gender" name="gender" placeholder="Gender" style="margin-top: 5px">
       <option value="male">Male</option>
       <option value="female">Female</option>
       <option value="other">Other</option>
     </select>
     <br></br>
-    <select id="accountType" placeholder="Account Type" style="margin-top: 5px">
+    <select id="accountType" name="accountType" placeholder="Account Type" style="margin-top: 5px">
           <option value="librarian">Librarian</option>
-          <option value="Author">Author</option>
-          <option value="Reader">Reader</option>
+          <option value="author">Author</option>
+          <option value="reader">Reader</option>
     </select>
     <br></br>
-    <button type="button" onclick="checkInputs()">
-      Sign Up
-    </button>
+    <input type="submit" name="signup" onclick="checkInputs()">
   </div>
-  <?php
-
-  ?>
+  </form>
 </body>
 </html>
 <script>
@@ -85,7 +156,7 @@
         errorMsg += "Please fill out the gender field. \n";
       }
       if ( document.getElementById('accountType').value == "" ) {
-              errorMsg += "Please fill out the acount type field. \n";
+              errorMsg += "Please fill out the account type field. \n";
       }
       if ( errorMsg != "" ) {
         alert(errorMsg);
@@ -102,7 +173,6 @@
     return false;
   }
   function checkInputs() {
-    checkForEmptyInputs();
-    checkPasswordConfirmation();
+    return (checkForEmptyInputs() && checkPasswordConfirmation());
   }
 </script>
