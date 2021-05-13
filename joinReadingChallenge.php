b8fbc23c7c318645729daed01602b9f5721bab8f
Furkan Baskaya <baskaya.furkan2000@gmail.com>
2021-05-13 20:19:32 +0300

    Update joinReadingChallenge.php

--- joinReadingChallenge.php
+++ joinReadingChallenge.php
@@ -8,33 +8,43 @@ if(!empty($_GET['selectedChallengeID'])){
     $selectedChallengeID = $_GET['selectedChallengeID'];
 }
 
-//display current challenges
-$getAllChallengesQuery = "SELECT * FROM reading_challenge";
-$getAllChallengesQueryResult = $mysqli->query($getAllChallengesQuery);
+//Get the selected challenge
+$getSelectedChallenge = "SELECT * FROM reading_challenge WHERE challenge_id = '$selectedChallengeID'";
+$getSelectedChallengeResult = $mysqli->query($getSelectedChallenge);
+$getSelectedChallengeRow = $getSelectedChallengeResult->fetch_assoc();
 
-if(isset($_POST['joinChallenge'])){
-    $selectedChallengeID = $_POST['joinChallenge'];
-    //Get the selected challenge
-    $getSelectedChallenge = "SELECT * FROM reading_challenge WHERE challenge_id = '$selectedChallengeID'";
-    $getSelectedChallengeResult = $mysqli->query($getSelectedChallenge);
-    $getSelectedChallengeRow = $getSelectedChallengeResult->fetch_assoc();
-}
 if(isset($_POST['takeChallenge'])){
     $userGoal = $_POST['bookGoal'];
     echo"Challenge ID: " . $selectedChallengeID;
     echo "User GOAL: " . $userGoal;
-    //Add new join relation tuple
-    $insertJoinsQuery = "INSERT INTO joins(user_id, challenge_id, goal) 
+    //Check if user previously joined
+    $searchUserQuery = "SELECT * FROM joins WHERE user_id = '$thisUserID' AND challenge_id = $selectedChallengeID";
+    $searchUserQueryResult = $mysqli->query($searchUserQuery);
+    if($searchUserQueryResult->num_rows == 1){
+        echo " You have joined this challenge before";
+    }
+    else{
+        //Add new join relation tuple
+        $insertJoinsQuery = "INSERT INTO joins(user_id, challenge_id, goal) 
                                 VALUES ('$thisUserID', '$selectedChallengeID', $userGoal)";
-    $insertJoinsQueryPrep = $mysqli->prepare($insertJoinsQuery);
-    $insertJoinsQueryResult = $insertJoinsQueryPrep->execute();
-    $insertJoinsQueryPrep->close();
-    //update reading challange pledged book count
-    $updateRCQuery = "UPDATE reading_challenge SET books_pledged = books_pledged + '$userGoal' WHERE reading_challenge.challenge_id = '$selectedChallengeID'";
-    $updateRCQueryPrep = $mysqli->prepare($updateRCQuery);
-    $updateRCQueryResult = $updateRCQueryPrep->execute();
-    $updateRCQueryPrep->close();
+        $insertJoinsQueryPrep = $mysqli->prepare($insertJoinsQuery);
+        $insertJoinsQueryResult = $insertJoinsQueryPrep->execute();
+        $insertJoinsQueryPrep->close();
+        //update reading challange pledged book count
+        $updateRCQuery = "UPDATE reading_challenge SET books_pledged = books_pledged + '$userGoal' WHERE reading_challenge.challenge_id = '$selectedChallengeID'";
+        $updateRCQueryPrep = $mysqli->prepare($updateRCQuery);
+        $updateRCQueryResult = $updateRCQueryPrep->execute();
+        $updateRCQueryPrep->close();
+        //update participant count
+        $updateRCQuery = "UPDATE reading_challenge SET participant_count = participant_count + 1 WHERE reading_challenge.challenge_id = '$selectedChallengeID'";
+        $updateRCQueryPrep = $mysqli->prepare($updateRCQuery);
+        $updateRCQueryResult = $updateRCQueryPrep->execute();
+        $updateRCQueryPrep->close();
+    }
 }
+//display current challenges
+$getAllChallengesQuery = "SELECT * FROM reading_challenge";
+$getAllChallengesQueryResult = $mysqli->query($getAllChallengesQuery);
 
 ?>
 <!DOCTYPE html>
@@ -63,8 +73,8 @@ if(isset($_POST['takeChallenge'])){
                     <td>" . $getAllChallengesQueryRow['books_pledged'] . "</td>
                     <td>" . $getAllChallengesQueryRow['time_limit'] . "</td>
                     <td>
-                        <form style='margin-top: 10px; margin-bottom: 5px' method='post' action='joinReadingChallenge.php?selectedChallengeID='" . urlencode($getAllChallengesQueryRow['challenge_id']) . ">
-                            <button class='btn' name='joinChallenge' value='".$getAllChallengesQueryRow['challenge_id']."'>Join</button>
+                        <form style='margin-top: 10px; margin-bottom: 5px' method='post' action='joinReadingChallenge.php?selectedChallengeID=" . urlencode($getAllChallengesQueryRow['challenge_id']) . "'>
+                            <button class='btn' name='joinChallenge'>Join</button>
                         </form>
                     </td>
                   </tr>";
@@ -75,9 +85,9 @@ if(isset($_POST['takeChallenge'])){
     </div>
     <div style="width: 49%; position: absolute; top: 0px; right: 50px;">
         <?php
-            if(!empty($selectedChallengeID)) {
-                //User is in his/her profile can create posts
-                echo "<h1>".$getSelectedChallengeRow['name']."</h1>
+        if(!empty($selectedChallengeID)) {
+            //User is in his/her profile can create posts
+            echo "<h1>".$getSelectedChallengeRow['name']."</h1>
                         <label>Participants: </label><b></b>".$getSelectedChallengeRow['participant_count']."
                         <br>
                         <label>Books Pledged: </label>".$getSelectedChallengeRow['books_pledged']."
@@ -85,13 +95,13 @@ if(isset($_POST['takeChallenge'])){
                         <label>Expire Date: </label>".$getSelectedChallengeRow['time_limit']."
                         <br>
                         ";
-                echo"<div style='border: 1px solid black; border-collapse: collapse; margin-top: 50px; width: 50%; padding: 20px;'>
+            echo"<div style='border: 1px solid black; border-collapse: collapse; margin-top: 50px; width: 50%; padding: 20px;'>
                      <form method='post'>
                         <h4>Number of books to pledge: <span><input type='number' name='bookGoal' style='width: 40px;'></span></h4>
                         <button class='btn' name='takeChallenge'>Take the Challenge</button>
                      </form>
                      </div>";
-            }
+        }
         ?>
     </div>
     <h1>
