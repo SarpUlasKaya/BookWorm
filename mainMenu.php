f1e53863a1b5d213330219edb67d291d87c121a0
Furkan Baskaya <baskaya.furkan2000@gmail.com>
2021-05-15 00:22:54 +0300

    Update mainMenu.php

--- mainMenu.php
+++ mainMenu.php
@@ -1,3 +1,17 @@
+<?php
+    session_start();
+    include("config.php");
+    $thisUserID = $_SESSION['userID'];
+
+    $checkReaderAccountType = "SELECT * FROM reader_account WHERE user_id = '$thisUserID'";
+    $resultReaderAcc = $mysqli->query($checkReaderAccountType);
+
+    $checkAuthorAccountType = "SELECT * FROM author_account WHERE user_id = '$thisUserID'";
+    $resultAuthorAcc = $mysqli->query($checkAuthorAccountType);
+
+    $checkLibrarianAccountType = "SELECT * FROM librarian_account WHERE user_id = '$thisUserID'";
+    $resultLibrarianAcc = $mysqli->query($checkLibrarianAccountType);
+?>
 <body>
 <section>
     <div style="align-items: center">
@@ -6,17 +20,41 @@
             <img  style="position: relative; max-height: 80px; max-width: 80px;" src="img/logo.png">
             <ul>
                 <li>
-                    <a style="margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="searchBooks.php">Search Book</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="addBook.php">Add Book</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="myLibrary.php">My Library</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="searchUsers.php">Search Users</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="userDetails.php">My Profile</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="createReadingChallenge.php">Create Reading Challenge</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="joinReadingChallenge.php">Join Reading Challenge</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="createQuiz.php">Create Quiz</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="quizTable.php">Solve Quiz</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="createRequest.php">Create Request</a>
-                    <a style="margin-left: 25px; display: inline-block; color: darkgrey;text-decoration: navajowhite;" href="viewRequest.php">View Request</a>
+                    <?php
+                        if($resultReaderAcc->num_rows==1){
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"searchBooks.php\">Search Book</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"myLibrary.php\">My Library</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"searchUsers.php\">Search Users</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"userDetails.php\">My Profile</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"joinReadingChallenge.php\">Join Reading Challenge</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"createQuiz.php\">Create Quiz</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"quizTable.php\">Solve Quiz</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"createRequest.php\">Create Request</a>";
+                        }
+                        elseif ($resultAuthorAcc->num_rows==1){
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"searchBooks.php\">Search Book</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"addBook.php\">Add Book</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"myLibrary.php\">My Library</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"searchUsers.php\">Search Users</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"userDetails.php\">My Profile</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"joinReadingChallenge.php\">Join Reading Challenge</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"createQuiz.php\">Create Quiz</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"quizTable.php\">Solve Quiz</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"createRequest.php\">Create Request</a>";
+                        }
+                        else{
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"searchBooks.php\">Search Book</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"myLibrary.php\">My Library</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"searchUsers.php\">Search Users</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"userDetails.php\">My Profile</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"createReadingChallenge.php\">Create Reading Challenge</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"joinReadingChallenge.php\">Join Reading Challenge</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"createQuiz.php\">Create Quiz</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"quizTable.php\">Solve Quiz</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"createRequest.php\">Create Request</a>";
+                            echo "<a style=\"margin-left: 10px; display: inline-block; color: darkgrey;text-decoration: navajowhite;\" href=\"viewRequest.php\">View Request</a>";
+                        }
+                    ?>
                 </li>
             </ul>
         </a>
